b5df8de58cfbcc5d47a18b56b69910f199b0049e
kotiltimgit <143795857+kotiltimgit@users.noreply.github.com>
2026-03-12 19:01:36 +0530

    Update appointment.sql

--- models/ingestion_framework/appointment/appointment.sql
+++ models/ingestion_framework/appointment/appointment.sql
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 {{
     config(
         materialized='dummy_materialization',
@@ -14,24 +14,5 @@
 
 {{{ file_archive_macro() }}}
 
+
 {{ audit_logging_update_macro() }}
-=======
-{{
-    config(
-        materialized='dummy_materialization',
-        alias='APPOINTMENT',
-        database=target.database,
-        schema='BRONZE'
-    )
-}}
-
-SELECT 1
-
-{#{ audit_logging_insert_macro() }#}
-
-{#{ ingestion_json_macro() }#}
-
-{#{{ file_archive_macro() }}#}
-
-{#{ audit_logging_update_macro() }#}
->>>>>>> ac6d9d8a5b20899cef8543877c85ce46e95878dd
